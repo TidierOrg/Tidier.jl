f418babac37dcaa54ac3f4b977095312773cb2e2
Karandeep Singh <kdpsingh@umich.edu>
2023-02-05 23:23:11 -0500

    Initial commit.

--- src/Tidier.jl
+++ src/Tidier.jl
@@ -1,5 +1,36 @@
 module Tidier
 
+using DataFrames
+import DataFramesMeta
+using MacroTools: postwalk, @capture
+
+export @autovec, @mutate, @select
+
 # Write your package code here.
+macro autovec(expr)
+  new_expr = postwalk(expr) do x
+    @capture(x, fn_(ex__)) || return x
+      if !(fn in [:mean :median :first :last :minimum :maximum])
+        # println(:($fn.($(ex...))))
+        return :($fn.($(ex...)))
+      else
+        return x
+      end
+    end
+  # println(new_expr)
+  new_expr
+end
+
+macro mutate(df, expr)
+  quote
+    DataFramesMeta.@transform($df, @autovec($expr))
+  end
+end
+
+macro select(df, expr)
+  quote
+    DataFramesMeta.@select($df, @autovec($expr))
+  end
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
-using Tidier
+using .Tidier
 using Test
 
+using DataFrames: DataFrame
+# using DataFramesMeta
+using Chain
+using Statistics
+
+df = DataFrame(a = 1:10, b = 11:20)
+
+@select(df, :a)
+
+@macroexpand @autovec(1+1)
+
 @testset "Tidier.jl" begin
-    # Write your tests here.
+  # Write your tests here.
 end
