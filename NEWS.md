4beb40294b3bfc6adf4149551ec41d3b063d7a46
Karandeep Singh <kdpsingh@umich.edu>
2023-02-09 11:57:48 -0500

    - Fixed bug with `@rename()` so that it supports multiple arguments - Added support for numerical selection (both positive and negative) to `@select()` - Added support for `@slice()`, including positive and negative indexing - Added support for `@arrange()`, including the use of `desc()` to specify descending order - Added support for `across()`, which has been confirmed to work with both `@mutate()`, `@summarize()`, and `@summarise()`.

---
+++ NEWS.md
@@ -0,0 +1,13 @@
+# Tidier.jl updates
+
+## 2023-02-09
+
+- Fixed bug with `@rename()` so that it supports multiple arguments
+- Added support for numerical selection (both positive and negative) to `@select()`
+- Added support for `@slice()`, including positive and negative indexing
+- Added support for `@arrange()`, including the use of `desc()` to specify descending order
+- Added support for `across()`, which has been confirmed to work with both `@mutate()`, `@summarize()`, and `@summarise()`.
+
+## 2023-02-07
+
+- Initial release
--- src/Tidier.jl
+++ src/Tidier.jl
@@ -2,13 +2,17 @@ module Tidier
 
 using DataFrames
 using MacroTools
+using Chain
 using Reexport
 
+@reexport using Chain
 @reexport using Statistics
 
-export @select, @transmute, @rename, @mutate, @summarize, @summarise, @filter, @group_by
+export @select, @transmute, @rename, @mutate, @summarize, @summarise, @filter, @group_by, @slice, @arrange
 
-# Write your package code here.
+# Non-exported helper functions
+# across(), desc()
+
 macro autovec(df, fn_name, exprs...)
 
   if fn_name == "groupby"
@@ -21,23 +25,64 @@ macro autovec(df, fn_name, exprs...)
 
   for expr in tp
  
+    # This creates :column => (x -> subset criteria) => :ignore,
+    # and then the "=> :ignore" portion is removed later on.
+    # Eventually may handle this more directly.
     if (fn_name == "subset")
       expr = :(ignore = $expr)
     end
 
     arr_rhs = String[]  
  
+    check_if_across = false
+
+    # Only auto-vectorize code outside of summarize/summarise,
+    # which have an fn_name of "combine"
+    # Auto-vectorize means that any of the functions *not*
+    # included in the array below are vectorized automatically.
     new_expr = MacroTools.postwalk(expr) do x
       @capture(x, fn_(ex__)) || return x
         push!(arr_rhs, join([ex...], ";"))
-        if !(fn in [:mean :median :first :last :minimum :maximum :sum :length :skipmissing])
-        # println(:($fn.($(ex...))))
-          return :($fn.($(ex...)))
-        else
-          return x
-        end
-    end
+        if fn == :across
+          vars_clean = string(ex[1])
+          vars_clean = split(vars_clean, ", ")
+          vars_clean = replace.(vars_clean, r"^\(" => s"")
+          for i in eachindex(vars_clean)
+            if !occursin(r"[()]", vars_clean[i])
+              vars_clean[i] = ":" * vars_clean[i]
+            elseif occursin(r"\)$", vars_clean[i]) && !occursin(r"\(", vars_clean[i])
+              vars_clean[i] = ":" * vars_clean[i]
+              vars_clean[i] = replace(vars_clean[i], r"\)$" => s"")
+            end
+          end
+          
+          vars_clean = "[" * join(vars_clean, ", ") * "]"
 
+          fns_clean = string(ex[2])
+          fns_clean = split(fns_clean, ", ")
+          fns_clean = replace.(fns_clean, r"^\(" => s"")
+          for i in eachindex(fns_clean)
+            if occursin(r"\)$", fns_clean[i]) && !occursin(r"\(", fns_clean[i])
+              fns_clean[i] = replace(fns_clean[i], r"\)$" => s"")
+            end
+          end
+          fns_clean = "[" * join(fns_clean, " ") * "]"
+
+          push!(arr_calls, vars_clean * " .=> " * fns_clean)
+          check_if_across = true
+        elseif fn_name == "combine" || (fn in [:mean :median :first :last :minimum :maximum :sum :length :skipmissing :quantile :passmissing :startswith :contains :endswith])
+          return x
+        else
+          return :($fn.($(ex...)))
+        end
+      end
+
+      if check_if_across
+        continue
+      end
+
+    # If there is no right-sided expression, then look for patterns of a = b,
+    # and push the b to the right side.
     if length(arr_rhs) == 0
       MacroTools.postwalk(expr) do x
         @capture(x, a_ = b_) || return x
@@ -47,6 +92,9 @@ macro autovec(df, fn_name, exprs...)
 
     arr_lhs = String[]
     
+    # Push any symbols (variables) on the left side of the `=` sign
+    # to the arr_lhs array, which contains the left-hand side arguments
+    # expr_lhs is currently ignored, so the returned values don't matter
     expr_lhs = MacroTools.postwalk(new_expr) do x
       @capture(x, s_Symbol = body_) || return x
       if !(s in [:+ :- :* :/ :\])
@@ -68,6 +116,7 @@ macro autovec(df, fn_name, exprs...)
       arr_rhs_match = match.(r"(-?)\(?([^\W0-9]\w*?)(:?)([^\W0-9]\w*)?\)?", arr_rhs)
       arr_rhs = arr_rhs[.!isnothing.(arr_rhs_match)]
       arr_rhs = unique(arr_rhs)
+      
       arr_rhs_symbols = string(Symbol.(arr_rhs))
       # arr_rhs = length(arr_rhs) > 1 ? reduce(vcat, arr_rhs) : arr_rhs
 
@@ -75,14 +124,19 @@ macro autovec(df, fn_name, exprs...)
       fn_body = join(strip.(split(fn_body, "=")[2:end]), "=")
       
       if (fn_name == "rename")
-        push!(arr_calls, arr_rhs_symbols * " .=> :" * arr_lhs) # vectorize bc array to scalar
+        push!(arr_calls, SubString(arr_rhs_symbols, 2, lastindex(arr_rhs_symbols) - 1) * " => :" * arr_lhs)
       elseif (fn_name == "subset")
         push!(arr_calls, arr_rhs_symbols * " => ((" * join(arr_rhs, ",") * ") -> " * fn_body * ")")
       else
         push!(arr_calls, arr_rhs_symbols * " => ((" * join(arr_rhs, ",") * ") -> " * fn_body * ") => :" * arr_lhs)
       end
     else
-      selection_match = match.(r"(-?)\(?\(?([^\W0-9]\w*)(:?)([^\W0-9]\w*)?\)?\)?", string(expr))
+
+      # selection_match = match.(r"(-?)\(?\(?([^\W0-9]\w*)(:?)([^\W0-9]\w*)?\)?\)?", string(expr))
+      
+      # Selection actually can be a number and doesn't have to be a valid variable name      
+      selection_match = match.(r"(-?)\(?\(?(\w+)(:?)(\w+)?\)?\)?", string(expr))
+      
       # arr_rhs = arr_rhs[.!isnothing.(arr_rhs_match)]
       
       # println(string(expr))
@@ -105,10 +159,19 @@ macro autovec(df, fn_name, exprs...)
     # println(expr_symbols)
   end
 
-  # fn_call = "$fn_name($df, " *  join(arr_calls, ",") * ")"
+  fn_call = "$fn_name($df, " *  join(arr_calls, ",") * ")"
+  
+  # @info fn_call
+
   # Meta.parse(fn_call)
+
   return_val = quote
+
+    # Ultimately we need to remove this eval() because this limits the use of functions
+    # to those re-exported by Tidier.jl
+
     arr_eval_calls = eval.(Meta.parse.($arr_calls))
+
     if $fn_name == "select"
       select($(esc(df)), arr_eval_calls...)
     elseif $fn_name == "rename"
@@ -175,6 +238,57 @@ macro group_by(df, exprs...)
   quote
     @autovec($(esc(df)), "groupby", $(exprs...))
   end
+end
+
+macro slice(df, exprs...)
+  quote
+    indices = [$(exprs...)]
+    try
+      # @info indices
+      if (all(indices .< 0))
+        # return_string = "$df[Not(" * string(-indices) * "), :]"
+        return_value = $(esc(df))[Not(-indices), :]
+      else
+        # return_string = "$df[" * string(indices) * ", :]"
+        return_value = $(esc(df))[indices, :]
+      end
+    catch e
+      # @info indices
+      indices = reduce(vcat, collect.(indices))  
+      if (all(indices .< 0))
+        # return_string = "$df[Not(" * string(-indices) * "), :]"
+        return_value = $(esc(df))[Not(-indices), :]
+      else
+        # return_string = "$df[" * string(indices) * ", :]"
+        return_value = $(esc(df))[indices, :]
+      end
+
+      # @info return_string
+      return return_value
+    end
+  end
+end
+
+macro arrange(df, exprs...)
+  tp = tuple(exprs...)
+  arr_calls = String[]
+
+  for expr in tp
+    expr_string = string(expr)
+    expr_string = replace(expr_string, r"^desc\((.+)\)$" => s"order(:\1, rev=true)")
+
+    if !occursin(r"[()]", expr_string)
+      expr_string = ":" * expr_string
+    end
+ 
+    push!(arr_calls, expr_string)
+  end
+
+  return_val = quote
+    arr_eval_calls = eval.(Meta.parse.($arr_calls))
+    sort($(esc(df)), [arr_eval_calls...])
+  end
+  return_val
 end
 
 end
