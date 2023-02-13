42ce980654b93e506d49c261f5d38765c6be0065
Zhe Zhao <zzhaozhe@umich.edu>
2023-02-13 17:20:07 -0500

    Initiate adding docstrings to each macro

--- src/Tidier.jl
+++ src/Tidier.jl
@@ -25,12 +25,12 @@ end
 macro autovec(df, fn_name, exprs...)
 
   if fn_name == "groupby"
-    fn_call = "groupby($df, :" *  join(exprs, ", :") * ")"  
-    
+    fn_call = "groupby($df, :" * join(exprs, ", :") * ")"
+
     # After :escape, there is either a symbol containing name of data frame
     # as in :movies, or if using @chain, then may say Symbol("##123"), so
     # colon is optional.
-  
+
     fn_call = replace(fn_call, r"^(.+)\$\(Expr\(:escape, :?(.+?)\)(.+)$" => s"\1\2\3")
     fn_call = replace(fn_call, r"Symbol\((\".+?\")\)+," => s"var\1,")
     println(fn_call)
@@ -43,7 +43,7 @@ macro autovec(df, fn_name, exprs...)
   arr_calls = String[]
 
   for expr in tp
- 
+
     # This creates :column => (x -> subset criteria) => :ignore,
     # and then the "=> :ignore" portion is removed later on.
     # Eventually may handle this more directly.
@@ -51,8 +51,8 @@ macro autovec(df, fn_name, exprs...)
       expr = :(ignore = $expr)
     end
 
-    arr_rhs = String[]  
- 
+    arr_rhs = String[]
+
     check_if_across = false
 
     # Only auto-vectorize code outside of summarize/summarise,
@@ -61,59 +61,59 @@ macro autovec(df, fn_name, exprs...)
     # included in the array below are vectorized automatically.
     new_expr = MacroTools.postwalk(expr) do x
       @capture(x, fn_(ex__)) || return x
-        push!(arr_rhs, join([ex...], ";"))
-        if fn == :across
-          vars_clean = string(ex[1])
-          vars_clean = split(vars_clean, ", ")
-          vars_clean = replace.(vars_clean, r"^\(" => s"")
-          for i in eachindex(vars_clean)
-            if !occursin(r"[()]", vars_clean[i])
-              vars_clean[i] = ":" * vars_clean[i]
-            elseif occursin(r"\)$", vars_clean[i]) && !occursin(r"\(", vars_clean[i])
-              vars_clean[i] = ":" * vars_clean[i]
-              vars_clean[i] = replace(vars_clean[i], r"\)$" => s"")
-            end
+      push!(arr_rhs, join([ex...], ";"))
+      if fn == :across
+        vars_clean = string(ex[1])
+        vars_clean = split(vars_clean, ", ")
+        vars_clean = replace.(vars_clean, r"^\(" => s"")
+        for i in eachindex(vars_clean)
+          if !occursin(r"[()]", vars_clean[i])
+            vars_clean[i] = ":" * vars_clean[i]
+          elseif occursin(r"\)$", vars_clean[i]) && !occursin(r"\(", vars_clean[i])
+            vars_clean[i] = ":" * vars_clean[i]
+            vars_clean[i] = replace(vars_clean[i], r"\)$" => s"")
           end
-          
-          vars_clean = "[" * join(vars_clean, " ") * "]"
-
-          fns_clean = string(ex[2])
-          fns_clean = split(fns_clean, ", ")
-          fns_clean = replace.(fns_clean, r"^\(" => s"")
-          for i in eachindex(fns_clean)
-            if occursin(r"\)$", fns_clean[i]) && !occursin(r"\(", fns_clean[i])
-              fns_clean[i] = replace(fns_clean[i], r"\)$" => s"")
-            end
-          end
-          fns_clean = "[" * join(fns_clean, ", ") * "]"
-
-          push!(arr_calls, vars_clean * " .=> " * fns_clean)
-          check_if_across = true
-        elseif fn_name == "combine" || (fn in [:mean :median :first :last :minimum :maximum :sum :length :skipmissing :quantile :passmissing :startswith :contains :endswith])
-          return x
-        elseif contains(string(fn), r"[^\W0-9]\w*$") # valid variable name
-          return :($fn.($(ex...)))
-        else # operator
-          fn_new = Symbol("." * string(fn))
-          return :($fn_new($(ex...)))
         end
-      end
 
-      if check_if_across
-        continue
+        vars_clean = "[" * join(vars_clean, " ") * "]"
+
+        fns_clean = string(ex[2])
+        fns_clean = split(fns_clean, ", ")
+        fns_clean = replace.(fns_clean, r"^\(" => s"")
+        for i in eachindex(fns_clean)
+          if occursin(r"\)$", fns_clean[i]) && !occursin(r"\(", fns_clean[i])
+            fns_clean[i] = replace(fns_clean[i], r"\)$" => s"")
+          end
+        end
+        fns_clean = "[" * join(fns_clean, ", ") * "]"
+
+        push!(arr_calls, vars_clean * " .=> " * fns_clean)
+        check_if_across = true
+      elseif fn_name == "combine" || (fn in [:mean :median :first :last :minimum :maximum :sum :length :skipmissing :quantile :passmissing :startswith :contains :endswith])
+        return x
+      elseif contains(string(fn), r"[^\W0-9]\w*$") # valid variable name
+        return :($fn.($(ex...)))
+      else # operator
+        fn_new = Symbol("." * string(fn))
+        return :($fn_new($(ex...)))
       end
+    end
+
+    if check_if_across
+      continue
+    end
 
     # If there is no right-sided expression, then look for patterns of a = b,
     # and push the b to the right side.
     if length(arr_rhs) == 0
       MacroTools.postwalk(expr) do x
         @capture(x, a_ = b_) || return x
-          push!(arr_rhs, string(b))
+        push!(arr_rhs, string(b))
       end
     end
 
     arr_lhs = String[]
-    
+
     # Push any symbols (variables) on the left side of the `=` sign
     # to the arr_lhs array, which contains the left-hand side arguments
     # expr_lhs is currently ignored, so the returned values don't matter
@@ -123,13 +123,13 @@ macro autovec(df, fn_name, exprs...)
         push!(arr_lhs, string(s))
         return QuoteNode(s)
       else
-        return(x)
+        return (x)
       end
     end
 
     # println(arr_lhs)
     # println(arr_rhs)
-    
+
     if length(arr_lhs) > 0
       arr_lhs = last(arr_lhs)
 
@@ -138,13 +138,13 @@ macro autovec(df, fn_name, exprs...)
       arr_rhs_match = match.(r"(-?)\(?([^\W0-9]\w*?)(:?)([^\W0-9]\w*)?\)?", arr_rhs)
       arr_rhs = arr_rhs[.!isnothing.(arr_rhs_match)]
       arr_rhs = unique(arr_rhs)
-      
+
       arr_rhs_symbols = string(Symbol.(arr_rhs))
       # arr_rhs = length(arr_rhs) > 1 ? reduce(vcat, arr_rhs) : arr_rhs
 
       fn_body = string(new_expr)
       fn_body = join(strip.(split(fn_body, "=")[2:end]), "=")
-      
+
       if (fn_name == "rename")
         push!(arr_calls, SubString(arr_rhs_symbols, 2, lastindex(arr_rhs_symbols) - 1) * " => :" * arr_lhs)
       elseif (fn_name == "subset")
@@ -155,12 +155,12 @@ macro autovec(df, fn_name, exprs...)
     else
 
       # selection_match = match.(r"(-?)\(?\(?([^\W0-9]\w*)(:?)([^\W0-9]\w*)?\)?\)?", string(expr))
-      
+
       # Selection actually can be a number and doesn't have to be a valid variable name      
       selection_match = match.(r"(-?)\(?\(?(\w+)(:?)(\w+)?\)?\)?", string(expr))
-      
+
       # arr_rhs = arr_rhs[.!isnothing.(arr_rhs_match)]
-      
+
       # println(string(expr))
 
       arr_call = ""
@@ -171,7 +171,7 @@ macro autovec(df, fn_name, exprs...)
       end
 
       if selection_match[1] == "-"
-        arr_call = "Not(" * arr_call * ")" 
+        arr_call = "Not(" * arr_call * ")"
       end
       # println(arr_call)
       push!(arr_calls, arr_call)
@@ -181,8 +181,8 @@ macro autovec(df, fn_name, exprs...)
     # println(expr_symbols)
   end
 
-  fn_call = "$fn_name($df, " *  join(arr_calls, ",") * ")"
-  
+  fn_call = "$fn_name($df, " * join(arr_calls, ",") * ")"
+
   # After :escape, there is either a symbol containing name of data frame
   # as in :movies, or if using @chain, then may say Symbol("##123"), so
   # colon is optional.
@@ -220,31 +220,117 @@ macro autovec(df, fn_name, exprs...)
   return_val
 end
 
+"""
+    @select(df, esprs...)
 
+Select variables in a DataFrame.
+
+# Arguments
+- `df`: A DataFrame.
+- `esprs...`: One or more unquoted variable names separated by commas. Variable names 
+         can also be used as their positions in the data, like `x:y`, to select 
+         a range of variables.
+
+# Examples
+```julia-repl
+Julia> using RDatasets
+Julia> movies = dataset("ggplot2", "movies")
+Julia> @chain movies begin
+  @select(Title, Year, Length, Budget, Rating)
+end
+Julia> @chain movies begin
+  @select(Title:Rating)
+end
+```
+"""
 macro select(df, exprs...)
   quote
     @autovec($(esc(df)), "select", $(exprs...))
   end
 end
+esprsesprs
+"""
+    @transmute(df, esprs...)
 
+Create a new DataFrame with only computed columns.
+
+# Arguments
+- `df`: A DataFrame.
+- `esprs...`: add new columns or replace values of existed columns using
+         `new_variable = values` syntax.
+
+# Examples
+```julia-repl
+Julia> using RDatasets
+Julia> movies = dataset("ggplot2", "movies")
+Julia> @chain movies begin
+  @mutate(Budget = Budget/1_000_000)
+end
+"""
 macro transmute(df, exprs...)
   quote
     @autovec($(esc(df)), "select", $(exprs...))
   end
 end
 
+"""
+    @rename(df, esprs...)
+
+Change the names of individual column names in a DataFrame. Users can also use @select()
+to rename and select columns.
+
+# Arguments
+- `df`: A DataFrame.
+- `esprs...`: Use `new_name = old_name` syntax to rename selected columns.
+
+# Examples
+```julia-repl
+julia> using RDatasets
+julia> movies = dataset("ggplot2", "movies")
+julia> @chain movies begin
+  @mutate(Budget = Budget/1_000_000)
+end
+"""
 macro rename(df, exprs...)
   quote
     @autovec($(esc(df)), "rename", $(exprs...))
   end
 end
 
+"""
+    @mutate(df, esprs...)
+  
+Create new columns as functions of existing columns. The results have the same number of
+rows as `df`.
+
+# Arguments
+- `df`: A DataFrame.
+- `esprs...`: add new columns or replace values of existed columns using
+         `new_variable = values` syntax.
+
+"""
 macro mutate(df, exprs...)
   quote
     @autovec($(esc(df)), "transform", $(exprs...))
   end
 end
 
+"""
+    @summarize(df, esprs...)
+    @summarize(gd, esprs...)
+    @summarise(df, esprs...)
+    @summarise(gd, esprs...)
+
+Create a new DataFrame with one row that summarizing all observations from the input DataFrame, 
+or the input GroupedDataFrame. 
+
+# Arguments
+- `df`: A DataFrame.
+- `gd`: A GroupedDataFrame.
+- `exprs...`: a `new_variable = function(old_variable)` pair. `function()` should be an agregate 
+         function that returns a vector of lenght 1. 
+
+"""
 macro summarize(df, exprs...)
   quote
     @autovec($(esc(df)), "combine", $(exprs...))
@@ -257,18 +343,49 @@ macro summarise(df, exprs...)
   end
 end
 
+"""
+    @filter(df::AbstractDataFrame, exprs...)
+
+Subset a DataFrame and return a copy of DataFrame where specified conditions are satisfied.
+
+# Arguments
+- `df`: A DataFrame.
+- `esprs...`: transformation(s) that produce vectors containing `true` or `false`.
+
+"""
 macro filter(df, exprs...)
   quote
     @autovec($(esc(df)), "subset", $(exprs...))
   end
 end
 
+"""
+    @group_by(df, cols)
+
+Return a `GroupedDataFrame` where operations are performed by groups specified by unique 
+sets of `cols`.
+
+# Arguments
+- `df`: A DataFrame.
+- `cols`: DataFrame columns to group by. Can be a single column name or a vector of column names. 
+"""
 macro group_by(df, exprs...)
   quote
     @autovec($(esc(df)), "groupby", $(exprs...))
   end
 end
 
+"""
+    @slice(df, esprs...)
+
+Select, remove or duplicate rows by indexing their integer positions.
+
+# Arguments
+- `df`: A DataFrame.
+- `esprs...`: integer row values. Use positive values to keep the rows, or negative values to drop.
+         Values provided must be either all positive or all negative, and they must be within the
+         range of DataFrames' row number.
+"""
 macro slice(df, exprs...)
   quote
     df_name = $(string(df))
@@ -284,7 +401,7 @@ macro slice(df, exprs...)
         return_value = $(esc(df))[copy(indices), :]
       end
     catch e
-      local indices2 = reduce(vcat, collect.(indices))  
+      local indices2 = reduce(vcat, collect.(indices))
       if (all(indices2 .< 0))
         return_string = df_name * "[Not(" * string(-indices2) * "), :]"
         return_value = $(esc(df))[Not(-copy(indices2)), :]
@@ -299,6 +416,16 @@ macro slice(df, exprs...)
   end
 end
 
+"""
+    @arrange(df, esprs...)
+
+Orders the rows of a DataFrame by the values of specified columns.
+
+# Arguments
+- `df`: A DataFrame.
+- `esprs...`: Variables, or functions of variables from the input DataFrame. Use `desc()` to sort
+         in descending order.
+"""
 macro arrange(df, exprs...)
   tp = tuple(exprs...)
   arr_calls = String[]
@@ -310,13 +437,13 @@ macro arrange(df, exprs...)
     if !occursin(r"[()]", expr_string)
       expr_string = ":" * expr_string
     end
- 
+
     push!(arr_calls, expr_string)
   end
 
-  fn_call = "sort($df, " *  join(arr_calls, ",") * ")"
+  fn_call = "sort($df, " * join(arr_calls, ",") * ")"
   fn_call = replace(fn_call, r"(##\d+)" => s"var\"\1\"")
-  
+
   println(fn_call)
 
   return_val = quote
