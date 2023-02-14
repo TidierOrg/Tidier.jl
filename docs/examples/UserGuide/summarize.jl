ce70f18e52649cf2f83fbd2de3eafd7dae28dad9
Karandeep Singh <kdpsingh@umich.edu>
2023-02-13 21:21:46 -0500

    Updated Project.toml back to DataFrames 1.5 and cleaned up markdown in summarize docs

--- docs/examples/UserGuide/summarize.jl
+++ docs/examples/UserGuide/summarize.jl
@@ -3,8 +3,10 @@ using DataFrames
 using RDatasets
 
 movies = dataset("ggplot2", "movies");
+
 # Both @summarize and @summarise can be used.
-df_summ = @chain movies begin
+
+@chain movies begin
     @filter(!ismissing(Budget))
     @summarize(nrow = length(Title))
 end
