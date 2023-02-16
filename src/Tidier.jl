9c15b1894979a6ed543c3cefa9f913a67a5cb40a
Zhe Zhao <zzhaozhe@umich.edu>
2023-02-15 23:09:46 -0500

    Declare methods for df1 and df2 arguments

--- src/Tidier.jl
+++ src/Tidier.jl
@@ -626,13 +626,13 @@ macro arrange(df, exprs...)
   return_val
 end
 
-function left_join(df1, df2; by=nothing)
+function left_join(df1::AbstractDataFrame, df2::AbstractDataFrame; by=nothing)
   # left join two DataFrames
   # This function takes a string or a vector of strings for `on` argument
   # First step, join two data frames using one column
   # Second step, consider when on is not specified, how to autojoin
   # Third step, take an expression
-  if typeof(by) == Expr
+  if by isa Expr
     str_by = replace(string(by), r"[\[\]]" => "")
     str_by_vec = split(str_by, ",")
 
@@ -653,7 +653,7 @@ function left_join(df1, df2; by=nothing)
         by = names(df1)[common_cols]
       end
     else
-      if typeof(by) == String
+      if by isa String
         by = push!([], by)
       end
     end
