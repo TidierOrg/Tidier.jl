dbc8186f92b58440cef56276d8e2d45c1b0f765a
Zhe Zhao <zzhaozhe@umich.edu>
2023-02-18 22:40:55 -0500

    clean up the src file

--- src/Tidier.jl
+++ src/Tidier.jl
@@ -9,7 +9,7 @@ using Reexport
 @reexport using Chain
 @reexport using Statistics
 
-export @select, @transmute, @rename, @mutate, @summarize, @summarise, @filter, @group_by, @slice, @arrange, across, desc, left_join
+export @select, @transmute, @rename, @mutate, @summarize, @summarise, @filter, @group_by, @slice, @arrange, @left_join, across, desc
 
 """
     across(variable[s], function[s])
