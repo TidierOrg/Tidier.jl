363d4dc34e698b1f1dd2e21dc7cc2b70a04e1e74
Randall Boyes <33524191+rdboyes@users.noreply.github.com>
2023-03-10 14:31:22 -0500

    Update pivots.jl
    significantly improves code readability and ability to add arguments using an argument dictionary

--- src/pivots.jl
+++ src/pivots.jl
@@ -7,10 +7,19 @@ macro pivot_wider(df, exprs...)
     tidy_exprs = parse_pivot_args.(tidy_exprs)
     expr_dict = Dict(x.args[2] => x.args[3] for x in tidy_exprs)
 
+    # we need to define a dictionary 
+    # to hold arguments in the format expected by unstack()
+    arg_dict = Dict{Symbol, Any}()
+
+    if haskey(expr_dict, QuoteNode(:values_fill))
+        arg_dict[:fill] = String(eval(expr_dict[QuoteNode(:values_fill)]))
+    end
+
     df_expr = quote
         unstack(DataFrame($(esc(df))), 
             $(expr_dict[QuoteNode(:names_from)]),
-            $(expr_dict[QuoteNode(:values_from)]))
+            $(expr_dict[QuoteNode(:values_from)]);
+            $(arg_dict)...)
     end
 
     if code[]
@@ -24,33 +33,28 @@ end
 $docstring_pivot_longer
 """
 macro pivot_longer(df, exprs...)
+    # take the expressions and return arg => value dictionary 
     tidy_exprs = parse_interpolation.(exprs)
     tidy_exprs = parse_pivot_args.(tidy_exprs)
     expr_dict = Dict(x.args[2] => x.args[3] for x in tidy_exprs)
 
-    if !haskey(expr_dict, QuoteNode(:(names_to))) && !haskey(expr_dict, QuoteNode(:(values_to)))
-        df_expr = quote
-            stack(DataFrame($(esc(df))), $(expr_dict[QuoteNode(:cols)]))
-        end
-    elseif (haskey(expr_dict, QuoteNode(:(names_to))) && haskey(expr_dict, QuoteNode(:(values_to))))
-        df_expr = quote
-            stack(DataFrame($(esc(df))), 
-                $(expr_dict[QuoteNode(:cols)]),
-                variable_name = $(expr_dict[QuoteNode(:names_to)]),
-                value_name = $(expr_dict[QuoteNode(:values_to)]))
-        end
-    elseif haskey(expr_dict, QuoteNode(:(names_to)))
-        df_expr = quote
-            stack(DataFrame($(esc(df))), 
-                $(expr_dict[QuoteNode(:cols)]),
-                variable_name = $(expr_dict[QuoteNode(:names_to)]))
-        end
-    elseif haskey(expr_dict, QuoteNode(:(values_to)))
-        df_expr = quote
-            stack(DataFrame($(esc(df))), 
-                $(expr_dict[QuoteNode(:cols)]),
-                value_name = $(expr_dict[QuoteNode(:values_to)]))
-        end
+    # we need to define a dictionary 
+    # to hold arguments in the format expected by stack()
+    arg_dict = Dict{Symbol, Any}()
+
+    # if names_to was specified, pass that argument to variable_name
+    if haskey(expr_dict, QuoteNode(:names_to))
+        arg_dict[:variable_name] = eval(expr_dict[QuoteNode(:names_to)])
+    end
+
+    # if values_to was specified, pass that argument to value_name
+    if haskey(expr_dict, QuoteNode(:values_to))
+        arg_dict[:value_name] = eval(expr_dict[QuoteNode(:values_to)])
+    end
+
+    # splat any specified arguments in to stack()
+    df_expr = quote
+        stack(DataFrame($(esc(df))), $(expr_dict[QuoteNode(:cols)]); $(arg_dict)...)
     end
 
     if code[]
@@ -59,3 +63,4 @@ macro pivot_longer(df, exprs...)
     
     return df_expr
 end
+
