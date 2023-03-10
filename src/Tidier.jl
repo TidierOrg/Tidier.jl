ddde1fbcb882ef1fdef910986efe45098194e3dc
Zhe Zhao <zzhaozhe@umich.edu>
2023-03-10 17:34:51 -0500

    fix the bug for subset macros

--- src/Tidier.jl
+++ src/Tidier.jl
@@ -314,3 +314,15 @@ macro pull(df, column)
 end
 
 end
+
+
+using .Tidier
+using Chain
+using DataFrames
+using MacroTools
+
+df = DataFrame(a = repeat('a':'e'), b = 1:5, c = 11:15);
+
+@chain df begin
+@filter(!(b >= mean(b)))
+end
