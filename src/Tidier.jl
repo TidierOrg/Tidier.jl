05980cb49e46d3c94ed99feacd94a98a2fcce5fe
Karandeep Singh <karandeep@gmail.com>
2024-04-19 18:26:30 -0700

    Adds TidierDB and TidierFiles, bump version to 1.4

--- src/Tidier.jl
+++ src/Tidier.jl
@@ -1,11 +1,18 @@
 module Tidier
 
+export DB
+
 using Reexport
 
 @reexport using TidierData
 @reexport using TidierPlots
 @reexport using TidierCats
 @reexport using TidierDates
+
+@reexport import TidierDB
+const DB = TidierDB
+
+@reexport using TidierFiles
 @reexport using TidierStrings
 @reexport using TidierText
 @reexport using TidierVest
